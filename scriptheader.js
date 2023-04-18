8f0f42951a82c4c81e880f2d67c1503bef687e3b
Cristian20020131 <cristian20020131@hotmail.com>
2023-04-18 08:54:09 -0300

    a

--- scriptheader.js
+++ scriptheader.js
@@ -1,15 +1,7 @@
-window.onscroll = function() {scrollFunction()};
+$(document).ready(function(){
 
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.height = "10%";
-    document.getElementById("header").style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-    document.getElementById("header").style.fontSize = "80%";
-  }
+  $(window).on("scroll touchmove", function () {
+    $('#header').toggleClass('tiny', $(document).scrollTop() > 0);
+  });
 
-  else {
-    document.getElementById("header").style.height = "15%";
-    document.getElementById("header").style.backgroundColor = "black";
-    document.getElementById("header").style.fontSize = "100%";
-  }
-}
+}); 
