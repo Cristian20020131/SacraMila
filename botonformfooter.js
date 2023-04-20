ac5ff874c6d67defefb93516ec9814c622746d36
Cristian20020131 <cristian20020131@hotmail.com>
2023-04-19 23:06:32 -0300

    a

--- botonformfooter.js
+++ botonformfooter.js
@@ -2,5 +2,5 @@ const btn_footer = document.querySelector("#botonfooter");
 
 btn_footer.addEventListener("click", function(e){
     e.preventDefault();   
-    alert("Su reserva ha sido recibida exitosamente")
+    alert("Su mensaje ha sido enviado exitosamente")
 });
